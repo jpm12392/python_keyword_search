dc09eac2a9f5463eab4bf0cc1426a2e97063a1a8
jay_maurya <jpm12392@gmail.com>
2023-07-15 23:08:00 +0530

    add try and exceptions class

--- keyword_search/views.py
+++ keyword_search/views.py
@@ -13,7 +13,7 @@ import pandas as pd
 ## keyword based search
 class SearchKeywordAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        # try:
+        try:
             keyword = self.request.GET.get('keyword', None)
             path_to_json = r"C:\Users\jpm12\Downloads\patent_jsons\patent_jsons"
             json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
@@ -23,6 +23,6 @@ class SearchKeywordAPIView(APIView):
                 context = {'status': False,'message': 'No Record Found!!!'}
 
             return Response(context, status=status.HTTP_200_OK)
-        # except Exception as e:
-        #     context = {'status': False, 'message': 'Something Went Wrong'}
-        #     return Response(context, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            context = {'status': False, 'message': 'Something Went Wrong'}
+            return Response(context, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
